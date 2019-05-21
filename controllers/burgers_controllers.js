70b07d628320ad36726cb29e657dd2b443f5bb23
verbash <iandavi@gmail.com>
2019-05-20 20:03:44 -0700

    setup MVC and file tree

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burgers = require("../models/burger.js");
+
+
+
+
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,22 @@
 var orm = require("../config/orm.js");
 
+var burgers = {
+    all: function(callback) {
+      orm.all("burgers", function(res) {
+        callback(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(newBurger, callback) {
+      orm.create("burgers", newBurger, function(res) {
+        callback(res);
+      });
+    },
+    update: function(eatBurger, callback) {
+      orm.update("burgers", eatBurger, function(res) {
+        calback(res);
+      });
+    }
+  };
+
+module.exports = burgers;
