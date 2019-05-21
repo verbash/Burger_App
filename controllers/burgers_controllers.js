dd89a50da8a1b686945620a92cdb5854c23656ea
verbash <iandavi@gmail.com>
2019-05-20 21:51:37 -0700

    UI built, still working on controller

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -4,6 +4,41 @@ var router = express.Router();
 
 var burgers = require("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burgers.all(function(data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burgers.create(req.body.burgers, function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    cat.update({
+      sleepy: req.body.sleepy
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
 
 
 
